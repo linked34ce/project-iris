c6822b80ae06f4f01cf45b80f2473af21bb82f59
Kuroki Raiha <78296405+linked34ce@users.noreply.github.com>
2025-09-27 20:20:27 +0900

    delete Assets/Scripts/Battle/UI directory

--- Assets/Scripts/Battle/UI/Interfaces/ICommandButton.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.Events;
-
-public interface ICommandButton
-{
-    void SubscribeEvent(UnityAction call);
-    void ClearEvent();
-    void ShowCommand();
-    void HideCommand();
-}
